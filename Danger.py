da0c9f06a0ab816b62e4254f72d5f87d51915c7c
Rob Babel <greatbotboy@gmail.com>
2022-05-25 16:49:26 -0400

    Initial

---
+++ Danger.py
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/env python3
+import os
+import stat
+import subprocess
+
+drives = [["d","/dev/sdd",0],["e","/dev/sde",0],["d","/dev/sdf",0]]
+
+def isblockdevice(path):
+  return os.path.exists(path) and stat.S_ISBLK(os.stat(path).st_mode)
+
+for x in drives:
+    BlkDev = isblockdevice(x[1])
+    if BlkDev == True and x[2] == 0:
+        x[2] = 1
+        print("Murder that drive!! "+x[1])
+        process = subprocess.run(['dd if=/dev/urandom of='+x[1]+' bs=1M', 'Even more output'], 
+                         stdout=subprocess.PIPE, 
+                         universal_newlines=True)
+        
+    else:
+        x[2] = 0
+        process
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DangerousDan
+Don't use this!!! You will wipe your drives!!!
+
+I am not reponsible for what you do with this.
+
+You have been warned.
