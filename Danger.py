37ecb264536ca1c9f67261759c79476f4ea5f62a
Rob Babel <greatbotboy@gmail.com>
2022-05-26 09:39:34 -0400

    Disk Eater

--- Danger.py
+++ Danger.py
@@ -1,22 +1,25 @@
-﻿#!/usr/bin/env python3
+﻿#!/bin/python3 python3
 import os
 import stat
 import subprocess
+import time
 
-drives = [["d","/dev/sdd",0],["e","/dev/sde",0],["d","/dev/sdf",0]]
+drives = [["d","/dev/sdd",0],["e","/dev/sde",0],["f","/dev/sdf",0],["g","/dev/sdg",0],["h","/dev/sdh",0],["i","/dev/sdi",0],["j","/dev/sdj",0],["k","/dev/sdk",0],["l","/dev/sdl",0],["m","/dev/sdm",0]]
 
 def isblockdevice(path):
   return os.path.exists(path) and stat.S_ISBLK(os.stat(path).st_mode)
 
-for x in drives:
-    BlkDev = isblockdevice(x[1])
-    if BlkDev == True and x[2] == 0:
-        x[2] = 1
-        print("Murder that drive!! "+x[1])
-        process = subprocess.run(['dd if=/dev/urandom of='+x[1]+' bs=1M', 'Even more output'], 
-                         stdout=subprocess.PIPE, 
-                         universal_newlines=True)
-        
-    else:
+print("Waiting for devices")
+while True:
+  
+  for x in drives:
+      BlkDev = isblockdevice(x[1])
+      if BlkDev == False:
         x[2] = 0
-        process
+      if BlkDev == True and x[2] == 0:
+        x[2] = 1
+        print('Got '+x[1]+", You have been granted summary distruction, thank you for your service.")
+        subprocess.call(['xterm', '-e', 'sudo umount '+x[1]+'?*'])
+        subprocess.Popen(['xterm', '-e', 'sudo shred -v -n7 -z '+x[1]])
+        print("Waiting for devices")
+  time.sleep(1)
