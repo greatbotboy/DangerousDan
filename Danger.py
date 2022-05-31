ba55199cfdad3d62dc61187ffe1c33ed46502048
Rob B <6046441+greatbotboy@users.noreply.github.com>
2022-05-31 14:57:07 -0400

    Handels SSDs better and auto drive definitions

--- Danger.py
+++ Danger.py
@@ -1,25 +1,39 @@
-﻿#!/bin/python3 python3
+#!/bin/python3 python3
 import os
 import stat
+import string
 import subprocess
 import time
 
-drives = [["b","/dev/sdb",0],["c","/dev/sdc",0],["d","/dev/sdd",0],["e","/dev/sde",0],["f","/dev/sdf",0],["g","/dev/sdg",0],["h","/dev/sdh",0],["i","/dev/sdi",0],["j","/dev/sdj",0],["k","/dev/sdk",0],["l","/dev/sdl",0],["m","/dev/sdm",0]]
+drives = [];
+
+letters = list(string.ascii_lowercase)
+letters.extend([i+b for i in letters for b in letters])
+
+for letter in letters:
+  if letter != 'a':
+    drives.append([letter,"/dev/sd"+letter,0,1])
+#print(drives)
 
 def isblockdevice(path):
   return os.path.exists(path) and stat.S_ISBLK(os.stat(path).st_mode)
 
-print("Waiting for devices")
 while True:
-  
+  print("Waiting for devices")
   for x in drives:
       BlkDev = isblockdevice(x[1])
       if BlkDev == False:
         x[2] = 0
       if BlkDev == True and x[2] == 0:
         x[2] = 1
-        print('Got '+x[1]+", You have been granted summary distruction, thank you for your service.")
-        subprocess.run(["umount", x[1]+'?*'])
-        subprocess.Popen(['shred', '-v', '-n7', '-z', x[1]])
-        print("Waiting for devices")
+        Spinner = subprocess.getoutput('cat /sys/block/sd'++'/queue/rotational')
+        x[3] = Spinner
+        if Spinner == '0':
+          print('Got SSD '+x[1]+", You have been granted summary distruction, thank you for your service.")
+          subprocess.run(["umount", x[1]+'?*'])
+          subprocess.Popen(['blkdiscard', '-z',  x[1]])
+        else:
+          print('Got Spinner '+x[1]+", You have been granted summary distruction, thank you for your service.")
+          subprocess.run(["umount", x[1]+'?*'])
+          subprocess.Popen(['shred', '-v', '-n7', '-z', x[1]])
   time.sleep(1)
